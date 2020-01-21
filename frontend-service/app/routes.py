d1c12084dee6bb843a494e4f4d7c0b6384ce74ca
Hamza Carew <hamzacarew@gmail.com>
2020-01-21 23:05:59 +0000

    Added frontend implementation for dash application on /charts route

--- frontend-service/app/routes.py
+++ frontend-service/app/routes.py
@@ -1,6 +1,9 @@
 from app import app
 import requests
+from dash import Dash
+import dash_html_components as html
 
+# App Routes
 @app.route('/')
 def index():
     res = requests.get("http://general-service/api/players")
@@ -14,3 +17,14 @@ def player_comparison():
     # todo: Render any additonal tags and return the page to the user 
 
     return {'Result' : 'You have hit the player comparison endpoint'}
+
+
+# Dash application
+dash_app1 = Dash(
+    __name__,
+    server=app,
+    routes_pathname_prefix='/charts/'
+)
+
+# Setup layout for dash application
+dash_app1.layout = html.Div("My 1st Dash app")
