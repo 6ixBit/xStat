d3868d4d713e5c2d4614c8a5fbbe334f029ae0b8
Hamza Carew <hamzacarew@gmail.com>
2020-01-25 13:43:38 +0000

    Added basic status code tests for every service

---
+++ frontend-service/test_microservice_endpoints.py
@@ -0,0 +1,26 @@
+import unittest
+from app import app
+
+class ApiTestCase(unittest.TestCase):
+    '''Test cases for frontend service endpoints'''
+
+    def test_index_route(self):
+        '''Endpoint should return a 200 on request'''
+        tester = app.test_client(self)
+        res = tester.get('/', content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+
+    def test_charts_route(self):
+        '''Endpoint should return a 200 on request'''
+        tester = app.test_client(self)
+        res = tester.get('/charts', content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+
+    def test_player_comparison_endpoint(self):
+        '''Endpoint should return a 200 on request'''
+        tester = app.test_client(self)
+        res = tester.get('/player-comparison', content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+        
+if __name__ == '__main__':
+    unittest.main()
