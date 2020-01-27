084fd6b08817d6aa5ad4b402c4170e5a2da97a09
Hamza Carew <hamzacarew@gmail.com>
2020-01-27 22:34:44 +0000

    Player stats for a particular competition now gets consumed by grab_data script

--- general-service/grab_data.py
+++ general-service/grab_data.py
@@ -17,23 +17,52 @@ headers = {
     "x-rapidapi-key": Config.RAPID_API_KEY
 }
 
-
-def grab_teams(league_id):
-    ''' Grab all teams from a specific league
-
-        @param (int) league_id
-        @return (int []) teams    
-    '''
+def get_teams(league_id: int) -> list:
+    ''' Grab all teams from a specific league '''
     global headers
     url = f"https://api-football-v1.p.rapidapi.com/v2/teams/league/{league_id}"
     response = requests.get(url, headers=headers).json()
 
+    # Parse teams from JSON response
     teams = [ value['teams'] for key,value in response.items() ]
 
     # Parse team_ids from each team within the league
-    team_ids = [ element['team_id'] for index, element in enumerate(teams[0]) ]
-       
+    team_ids = [ element['team_id'] for index,element in enumerate(teams[0]) ]     
     return team_ids
+
+def get_players_id(team_id: int, season: str):
+    ''' Get stats for all players of each team passed as a parameter OR get all player ids for a team and return it in a list
+        @param (int) team_id: int
+        @param (YYYY or YYYY-YYYY : 2019 or 2019-2020) season: str
+    '''
+    global headers
+    url = f"https://api-football-v1.p.rapidapi.com/v2/players/squad/{team_id}/{season}"
+    
+    response = requests.get(url, headers=headers).json()
+
+    # todo: parse player ids from response and return all in a list
+    players = [ value['players'] for key, value in response.items() ]
+
+    player_ids = [ element['player_id'] for index, element in enumerate(players[0]) ]
+    return player_ids
+
+def get_player_stats(player_id: int, league="Premier League"):
+    ''' GET stats for each player and filter by params on link
+        @param (644) player_id - Player id of a specific player
+        @param ("Premier League") league - What league to filter stats by 
+    '''
+    global headers
+    url = f"https://api-football-v1.p.rapidapi.com/v2/players/player/{player_id}"
+    
+    response = requests.get(url, headers=headers).json()
+
+    players = [ value['players'] for key, value in response.items() ]
+
+    # Get stats for a particular competition based on league param passed       
+    player_stats = [ element[index] for index,element in enumerate(players) if element[index]['league'] == league ]
+
+    return player_stats
+    
   
 if __name__ == '__main__':
     # todo: Call function to grab data from API
@@ -41,4 +70,7 @@ if __name__ == '__main__':
     # pprint(list(result))
 
     league_id = leagues['Premier League']
-    pprint(grab_teams(league_id))
+    #pprint(get_teams(league_id))
+    #pprint(get_players_id(50, '2019-2020'))
+    pprint(get_player_stats(644))
+
