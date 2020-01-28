88404287e2df0da3fda6472cee8eace552b15285
Hamza Carew <hamzacarew@gmail.com>
2020-01-28 22:01:55 +0000

    Multiple player stats get submitted to database

--- general-service/grab_data.py
+++ general-service/grab_data.py
@@ -4,6 +4,7 @@ import requests
 import csv
 import pandas as pd
 from pprint import pprint
+import pymongo
 
 # All 5 leagues and their respective ids from the API
 leagues = {
@@ -32,7 +33,7 @@ def get_teams(league_id: int) -> list:
     team_ids = [ element['team_id'] for index,element in enumerate(teams[0]) ]     
     return team_ids
 
-def get_players_id(team_id: int, season: str):
+def get_players_id(team_id, season="2019-2020"):
     ''' Get stats for all players of each team passed as a parameter OR get all player ids for a team and return it in a list
         @param (int) team_id: int
         @param (YYYY or YYYY-YYYY : 2019 or 2019-2020) season: str
@@ -68,48 +69,48 @@ def get_player_stats(player_id: int, league="Premier League"):
 def write_stats_to_csv(player_stat, filename="player_stats.csv"):
     # Map users passed as arg into dict
     player = {
-        "player_id" : player_stat[0]['player_id'],
-        "player_name": player_stat[0]['player_name'],
-        "first_name": player_stat[0]['firstname'],
-        "last_name": player_stat[0]['lastname'],
-        "shirt_number": player_stat[0]['number'],
-        "position": player_stat[0]['position'],
-        "age": player_stat[0]['age'],
-        "team_name": player_stat[0]['team_name'],
-        "rating": player_stat[0]['rating'],
-        "nationality": player_stat[0]['nationality'],
-        "height": player_stat[0]['height'],
-        "weight": player_stat[0]['weight'],
-        "competition": player_stat[0]['league'],
-        "season": player_stat[0]['season'],
-        "total_shots": player_stat[0]['shots']['total'],
-        "shots_on_target": player_stat[0]['shots']['on'],
-        "goals": player_stat[0]['goals']['total'],
-        "assists": player_stat[0]['goals']['assists'],
-        "total_passes": player_stat[0]['passes']['total'],
-        "key_passes": player_stat[0]['passes']['key'],
-        "pass_accuracy": player_stat[0]['passes']['accuracy'],
-        "completed_tackles": player_stat[0]['tackles']['total'],
-        "blocks": player_stat[0]['tackles']['blocks'],
-        "interceptions": player_stat[0]['tackles']['interceptions'],
-        "dribble_attempts": player_stat[0]['dribbles']['attempts'],
-        "dribble_success": player_stat[0]['dribbles']['success'],
-        "fouls_drawn": player_stat[0]['fouls']['drawn'],
-        "fouls_commited": player_stat[0]['fouls']['committed'],
-        "yellow_cards": player_stat[0]['cards']['yellow'],
-        "red_cards": player_stat[0]['cards']['red'],
-        "penalty_won": player_stat[0]['penalty']['won'],
-        "penalty_commited": player_stat[0]['penalty']['commited'],
-        "penalty_success": player_stat[0]['penalty']['success'],
-        "penalty_missed": player_stat[0]['penalty']['missed'],
-        "appearences": player_stat[0]['games']['appearences'],
-        "minutes_played": player_stat[0]['games']['minutes_played'],
-        "match_starts": player_stat[0]['games']['lineups'],
-        "subbed_in": player_stat[0]['substitutes']['in'],
-        "subbed_out": player_stat[0]['substitutes']['out'],
-        "benched": player_stat[0]['substitutes']['bench'],
-        "total_duels": player_stat[0]['duels']['total'],
-        "duels_won": player_stat[0]['duels']['won']
+        "player_id" : player_stat['player_id'],
+        "player_name": player_stat['player_name'],
+        "first_name": player_stat['firstname'],
+        "last_name": player_stat['lastname'],
+        "shirt_number": player_stat['number'],
+        "position": player_stat['position'],
+        "age": player_stat['age'],
+        "team_name": player_stat['team_name'],
+        "rating": player_stat['rating'],
+        "nationality": player_stat['nationality'],
+        "height": player_stat['height'],
+        "weight": player_stat['weight'],
+        "competition": player_stat['league'],
+        "season": player_stat['season'],
+        "total_shots": player_stat['shots']['total'],
+        "shots_on_target": player_stat['shots']['on'],
+        "goals": player_stat['goals']['total'],
+        "assists": player_stat['goals']['assists'],
+        "total_passes": player_stat['passes']['total'],
+        "key_passes": player_stat['passes']['key'],
+        "pass_accuracy": player_stat['passes']['accuracy'],
+        "completed_tackles": player_stat['tackles']['total'],
+        "blocks": player_stat['tackles']['blocks'],
+        "interceptions": player_stat['tackles']['interceptions'],
+        "dribble_attempts": player_stat['dribbles']['attempts'],
+        "dribble_success": player_stat['dribbles']['success'],
+        "fouls_drawn": player_stat['fouls']['drawn'],
+        "fouls_commited": player_stat['fouls']['committed'],
+        "yellow_cards": player_stat['cards']['yellow'],
+        "red_cards": player_stat['cards']['red'],
+        "penalty_won": player_stat['penalty']['won'],
+        "penalty_commited": player_stat['penalty']['commited'],
+        "penalty_success": player_stat['penalty']['success'],
+        "penalty_missed": player_stat['penalty']['missed'],
+        "appearences": player_stat['games']['appearences'],
+        "minutes_played": player_stat['games']['minutes_played'],
+        "match_starts": player_stat['games']['lineups'],
+        "subbed_in": player_stat['substitutes']['in'],
+        "subbed_out": player_stat['substitutes']['out'],
+        "benched": player_stat['substitutes']['bench'],
+        "total_duels": player_stat['duels']['total'],
+        "duels_won": player_stat['duels']['won']
     }
 
     field_names = [ col for col in player.keys() ]
@@ -117,23 +118,44 @@ def write_stats_to_csv(player_stat, filename="player_stats.csv"):
     with open(filename, mode='w') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=field_names)
         writer.writeheader()
-
         writer.writerow(player)
+        # writer.writerows(player)
 
-    return pd.read_csv('player_stats.csv').head()
+    #return pd.read_csv('player_stats.csv').head()
 
-    
-  
+def write_to_db(player_stat):
+    ''' Accepts a player stat object in list format and inserts it into database'''
+    my_client = pymongo.MongoClient(Config.MONGO_URL)
+
+    my_db = my_client['xStat']
+    my_collection = my_db['player_stats_general_service']
+
+    player = my_collection.insert_one(player_stat)
+    print(player.inserted_id)
+
 if __name__ == '__main__':
     # todo: Call function to grab data from API
-    # result = map(grab_teams, leagues.values()) -> RUN grab_teams function for every league
-    # pprint(list(result))
+    #team_ids = get_teams(leagues['Premier League']) - Too much data when testing - SWAP when commiting to database
+    team_ids = [46] #46
 
-    # team_ids = map(get_teams, leagues.values())
-    # pprint(list(team_ids))
+    player_ids = list(map(get_players_id, team_ids))
+    player_ids_pool = [ inner for outer in player_ids for inner in outer ] # Holds pool of player ids from a particular team.
 
-    my_player = get_player_stats(644)
-    print(write_to_csv(my_player))
+    player_stats = list(map(get_player_stats, player_ids_pool))
+    player_stats_pool = [ inner for outer in player_stats for inner in outer ] # Holds JSON of every player of a particular team
+
+    for player in player_stats_pool:
+        write_to_db(player)
+
+
+
+
+
+    
+
+
+
+    
 
     
 
