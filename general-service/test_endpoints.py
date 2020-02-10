56d7cbc1fb01e6e8c834525655219f8d0a52c06b
Hamza Carew <hamzacarew@gmail.com>
2020-02-10 20:49:14 +0000

    Modified error checking test for wrong incorrect url route

--- general-service/test_endpoints.py
+++ general-service/test_endpoints.py
@@ -5,10 +5,10 @@ class ApiTestCase(unittest.TestCase):
     '''Test cases for endpoints on /general service'''
 
     def test_api_goals_route(self):
-        '''Endpoint should return a 200 on request'''
+        '''Endpoint should return a 400 on request as URL is invalid'''
         tester = app.test_client(self)
-        res = tester.get('/api/stats/goals/Premier League', content_type='application/json')
-        self.assertEqual(res.status_code, 200)
+        res = tester.get('/api/stats/goals/fake route', content_type='application/json')
+        self.assertEqual(res.status_code, 400)
         
 
 if __name__ == '__main__':
