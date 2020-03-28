ed0ee54420c1d4c9ccd04e128146cc8069047a44
Hamza Carew <hamzacarew@gmail.com>
2020-03-28 18:00:19 +0000

    Names typed into search box in frontend now successfully grabs matched results from a container and logs it to the console

--- frontend-service/src/components/playerComparison/SearchBar.vue
+++ frontend-service/src/components/playerComparison/SearchBar.vue
@@ -1,21 +1,28 @@
 <template>
   <div>
        <b-form-input @input="OnChange"  v-model="text" placeholder="Enter player name" id="search_player"></b-form-input>
-    <div class="mt-2">Value: {{ text }}</div>
+    <div class="mt-2">Value: {{ text }} </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
   export default {
     data() {
       return {
-        text: ''
+        text: '',
+        player_names: []
       }
     },
     methods: {
-        OnChange(){
+        async OnChange(){
             //@desc Make AJAX call to backend for search potential results
-            console.log(this.text)
+            const matched_players = await axios.get(`http://localhost:8082/api/v1/players/search/${this.text}`)
+
+            // Set response from AJAX request to component state data
+            // https://alligator.io/vuejs/vue-autocomplete-component/
+            console.log(matched_players)
         }
     }
   }
--- player-comparison-service/src/__init__.py
+++ player-comparison-service/src/__init__.py
@@ -2,9 +2,12 @@
 
 # Thid party imports
 from flask import Flask, Blueprint
+from flask_cors import CORS
+
 
 # Setup application instance
 app = Flask(__name__)
+CORS(app)
 
 # Local imports
 from src.api.routes import mod as mod_api
