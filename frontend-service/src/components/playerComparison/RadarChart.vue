494ab41ab3210178208ed5fcab176701ec1954e4
Hamza Carew <hamzacarew@gmail.com>
2020-04-04 15:32:20 +0100

    Refactored codebase & implemented radar chart for player comparison service

--- frontend-service/src/components/playerComparison/RadarChart.vue
+++ frontend-service/src/components/playerComparison/RadarChart.vue
@@ -1,78 +1,109 @@
 <template>
-    <div> 
-        <!-- Res: {{ playerInfo }} -->
-        <!-- <radar-chart type="radar" height="350" :options="chartOptions" :series="chartSeries"/> -->
-        Length of Player Info: {{ this.playerInfo.length }} <br>
-        Radar: {{ this.chartSeries }}
-
-    </div>
+  <div>
+    Length of Player Info: {{ this.playerInfo.length }} <br />
+    <radar-chart type="radar" height="350" :options="chartOptions" :series="chartSeries"/>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-// import VueApexCharts from 'vue-apexcharts'
+import axios from "axios";
+import VueApexCharts from 'vue-apexcharts'
 
 export default {
-    data() {
-        return {
-            chartSeries: [],
-            chartOptions: {},
-            playerInfo: []
-        }
-    },
-    components: {
-        // 'radarChart' : VueApexCharts
-    },
-    computed: {
-        selectedPlayers(){
-            return this.$store.state.selectedPlayers;
-        }
-    },
-    methods: {
-        async getPlayers(players){
-            // Clear playerInfo to remove duplicates
-            this.clearPlayerInfo()
-
-            try {
-               for (var player of players) {
-                   const info = await axios.get(`http://localhost:8082/api/v1/players/${player}`)
-
-                   this.playerInfo.push(info.data[0])
-               }
-           }  catch (error) {
-               console.log(`Failed: ${error}`)
-           }
-        },
-        async clearPlayerInfo() {
-            this.playerInfo = []
-        },
-        async clearRadarChart() {
-            this.chartOptions = {};
-            this.chartSeries = [];
-        },
-        updateRadarChart() {
-            // Clear Radar Chart Data
-            this.clearRadarChart()
-
-            for (var player of this.playerInfo) { // NOT WORKING 
-                    let newSeries = {
-                        name: `${player.player_name} - ${player.season}`,
-                        data: [player.dribbleSuccess_per90, player.goals_per90, 
-                                player.keyPasses_per90, player.shots_on_target_p90,
-                                player.assists_per90]
-                            }
-                    this.chartSeries.push(newSeries)
-            }
-        }
-    },
-    watch: {
-        selectedPlayers(newData) { 
-            // Add players to central repo
-            this.getPlayers(newData)
-            .then( () => { this.updateRadarChart() }) // Update Charts.
-
-        }
+  data() {
+    return {
+      chartSeries: [],
+      chartOptions: {},
+      playerInfo: []
+    };
+  },
+  components: {
+    'radar-chart' : VueApexCharts
+  },
+  computed: {
+    selectedPlayers() {
+      return this.$store.state.selectedPlayers;
     }
-}
-</script>
+  },
+  methods: {
+    async getPlayers(players) {
+      // Clear playerInfo to remove duplicates
+      this.clearPlayerInfo();
 
+      try {
+        for (var player of players) {
+          const info = await axios.get(
+            `http://localhost:8082/api/v1/players/${player}`
+          );
+
+          this.playerInfo.push(info.data[0]);
+        }
+      } catch (error) {
+        console.log(`Failed: ${error}`);
+      }
+    },
+    async clearPlayerInfo() {
+      this.playerInfo = [];
+    },
+    async clearRadarChart() {
+      this.chartOptions = {};
+      this.chartSeries = [];
+    },
+    updateRadarChart(players) {
+      // Clear Radar Chart Data
+      this.clearRadarChart();
+
+      for (var player of players) {
+        let newSeries = {
+          name: `${player.player_name} - ${player.season}`,
+          data: [
+            player.dribbleSuccess_per90,
+            player.goals_per90,
+            player.keyPasses_per90,
+            player.shots_on_target_p90,
+            player.assists_per90
+          ]
+        };
+        this.chartSeries.push(newSeries);
+      }
+
+      // Set Chart Options
+      this.chartOptions = {
+        chart: {
+          height: 350,
+          type: "radar",
+          dropShadow: {
+            enabled: true,
+            blur: 1,
+            left: 1,
+            top: 1
+          }
+        },
+        title: {
+          text: `xStat Comparison Matrix`
+        },
+        stroke: {
+          width: 0
+        },
+        fill: {
+          opacity: 0.4
+        },
+        markers: {
+          size: 0
+        },
+        xaxis: {
+          categories: ["Dribbles", "Goals", "Key Passes", "Shots On Target", "Assists"]
+        }
+      };
+    }
+  },
+  watch: {
+    selectedPlayers(newData) {
+      // Fetch searched players then update chart
+      this.getPlayers(newData).then(() => {
+        this.updateRadarChart(this.playerInfo);
+      });
+    }
+  }
+};
+</script>
--- frontend-service/src/store/store.js
+++ frontend-service/src/store/store.js
@@ -1,17 +1,13 @@
 // Third party imports
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
 
 // Init Vuex
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state: {
-        radarChartSeries: [], // HOLDS ACTUAL CHART DATA
-        radarChartOptions: {}, // HOLDS CHART OPTIONS
-        selectedPlayers: [], // PLAYER NAMES OF THOSE SELECTED
-        playerInfo: [] // HOLDS ALL RELEVANT JSON FOR CURRENT SELECTED PLAYERS 
+        selectedPlayers: [] // PLAYER NAMES OF THOSE SELECTED
     },
     mutations: {
         removePlayer(state, playerName){
@@ -33,42 +29,13 @@ export const store = new Vuex.Store({
             } else {
                 state.selectedPlayers.push(player.player_name)
             }
-        },
-        clearPlayerInfo(state) {
-            // @desc Clear playerInfo object
-            state.playerInfo = [];
-        },
-        clearRadarChart(state) {
-            state.radarChartSeries = []
-            state.radarChartOptions = {}
-        },
-        updateRadarChart(state) {
-            // Loop players and add data to series
-            state.playerInfo.forEach( (player) => {
-              
-                let series = {
-                    name: `${player.player_name} - ${player.season}`,
-                    data: [player.dribbleSuccess_per90, player.goals_per90, 
-                            player.keyPasses_per90, player.shots_on_target_p90,
-                            player.assists_per90]
-                }
-                state.radarChartSeries.push(series)
-            })
-        }
-    },
-    actions: {
-        async getPlayers(context) {
-            // Clear player Info to avoid duplicates
-            // context.commit('clearPlayerInfo') GIVES ISSUES
-           try {
-               for (var player of context.state.selectedPlayers) {
-                   const info = await axios.get(`http://localhost:8082/api/v1/players/${player}`)
-
-                   this.state.playerInfo.push(info.data[0])
-               }
-           }  catch (error) {
-               console.log(error)
-           }
         }
     }
 }) 
+
+
+
+
+
+
+
